065f5bedf3e1e6b7c81a91b1c8ed16e9e099852a
Joao Eduardo <joaoeduardo616@yahoo.com>
2022-04-20 16:10:00 -0300

    Alterando Scroll

--- Projeto-1/main.js
+++ Projeto-1/main.js
@@ -1,62 +1,87 @@
-/* ScrollReveal Mostrar elementos quando der scroll na pagina*/
+
+
+
+
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+
+
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distatace: '30px',
-    duarion: 700,
-    reset: true 
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
 })
 
 scrollReveal.reveal(
-    '#home .image, #home .text,                                           #about .image, #about .text,                                    #services header, #services .card,                                    #testimonials header, #testimonials .testimonials                   #contact .text, #contact .links'
-    ,
-     { interval: 100})
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social, footer .brand, footer`, 
+
+  { interval: 100 }
+)
 
 
-/* Testimonials carouset slider swiper*/
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    pagination: {
-      el: '.swiper-pagination'
-    },
-    mousewheel: true,
-    keyboard: true
-  })
-   
 
-/*mudar o herader da pagina quando der scroll*/
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
-window.addEventListener('scroll', function () {
-    if (window.scrollY >= navHeight) {
-        //mair que a altura do header
-        header.classList.add('scroll');
-    } else {
-        //Menor que a altura do header
-        header.classList.remove('scroll');
-    }
-})
+function backToTopButton () {
+ const backToTopButton = document.querySelector('.back-to-top')
 
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+      } else {
+        backToTopButton.classList.remove('show')
+      }
+} 
 
-/* Abre e fecha o menu ao clilcar em cima do icone: hambuerger e X*/
-
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll("nav, toggle")
-
-for(const element of toggle){
-    element.addEventListener('click', function () {
-        nav.classList.toggle('show')
+    window.addEventListener('scroll', function() {
+        changeHeaderWhenScroll()
+        backToTopButton()
     })
-}
-/* Quando clicar em um item do menu escondor o menu*/
-
-const links = document.querySelectorAll('nav ul li a')
-
-for(const link of links) {
-    links.addEventListener('click', function () {
-        nav.classList.remove('show')
-
-    })
-}
 
 
     
